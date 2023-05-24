60bd1538a58ad25601e59196abe9ef515b17c2c5
youcefhedibel <youyou_dz1@live.fr>
2023-05-24 15:02:24 +0100

    Initial Commit

---
+++ TicTacToeApp/TicTacToeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeAppApp.swift
+//  TicTacToeApp
+//
+//  Created by mac on 24/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
